0920424cb981c0213c5f2634d4e975304eff0e8f
DongKi Lee <ki7348@naver.com>
2021-07-23 23:03:51 +0900

    first commit

--- ToyProject/03/src/store/CartProvider.js
+++ ToyProject/03/src/store/CartProvider.js
@@ -54,8 +54,9 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount
     };
   }
-  if(action.type === 'CLEAR'){
-      defaultCartState
+
+  if (action.type === 'CLEAR') {
+    return defaultCartState;
   }
 
   return defaultCartState;
@@ -72,19 +73,19 @@ const CartProvider = (props) => {
   };
 
   const removeItemFromCartHandler = (id) => {
-    dispatchCartAction({ type: 'REMOVE', id: id });
+    dispatchCartAction({ type: 'REMOVE', id: id }); 
   };
 
-  const clearCartHandler = () =>{
+  const clearCartHandler = () => {
     dispatchCartAction({type: 'CLEAR'});
-  }
+  };
 
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
-    clearCart: clearCartHandler,
+    clearCart: clearCartHandler
   };
 
   return (
